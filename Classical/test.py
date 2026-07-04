e425cedcad2f9891acf030d857394481f9bce8b4
Γιώργος Κοτίνης <gkotinis@csd.auth.gr>
2026-07-04 14:59:36 +0300

    Cleaning test.py

--- Classical/test.py
+++ Classical/test.py
@@ -38,8 +38,8 @@ def experiment(n: int, tau: float, theta: float, error_rate: float, runs: int = 
     success = 0
 
     for _ in range(runs):
-        real_values = generate_real_values_adversarial(n)
-        predictions = generate_predicted_values_adversarial(real_values, error_rate=error_rate)
+        real_values = generate_real_values_uniform(n)
+        predictions = generate_predicted_values_uniform(real_values, error_rate)
 
         best_real_candidate = max(range(n), key=lambda i: real_values[i]) # best candidate according to real values
 
@@ -55,40 +55,39 @@ def experiment(n: int, tau: float, theta: float, error_rate: float, runs: int = 
 if __name__ == "__main__":
     n = int(input("Πόσοι υποψήφιοι; "))
 
-    thresholds = [0.3, 0.5, 0.7]
+    thetas = [0.1, 0.3, 0.5, 0.7, 0.9]
     tau = 0.313
     error_rates = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
     runs = 10000
 
     fig = go.Figure()
 
-    for theta in thresholds:
-        print(f"\n--- Threshold: {theta} ---")
+    for theta in thetas:
+        print(f"\n--- Theta: {theta} ---")
 
         x_error_rates = []
-        y_competitive_ratios = []
+        y_success_rates = []
         for error_rate in error_rates:
             print(f"\n--- Error rate: {error_rate} ---")
             competitive_ratio, success_rate = experiment(n, tau, theta, error_rate, runs)
             print(f"competitive ratio: {competitive_ratio:.3f} ---- success rate: {success_rate:.3f}")
         
             x_error_rates.append(error_rate)
-            y_competitive_ratios.append(competitive_ratio)
+            y_success_rates.append(success_rate)
 
-        # Γραμμή Competitive Ratio για το συγκεκριμένο theta
         fig.add_trace(go.Scatter(
             x=x_error_rates, 
-            y=y_competitive_ratios,
+            y=y_success_rates,
             mode='lines+markers',
-            name=f'Comp. Ratio (θ={theta})',
-            hovertemplate=f'<b>θ={theta}</b><br>Error: %{{x}}<br>Ratio: %{{y:.3f}}<extra></extra>'
+            name=f'Success Rate (θ={theta})',
+            hovertemplate=f'<b>θ={theta}</b><br>Error: %{{x}}<br>Rate: %{{y:.3f}}<extra></extra>'
         ))
 
     # Ρυθμίσεις εμφάνισης 
     fig.update_layout(
-        title=f"Σύγκριση Στρατηγικών για το Πρόβλημα του Γραμματέα (n={n}, τ={tau})",
-        xaxis_title="Error Rate (Ρυθμός Σφάλματος)",
-        yaxis_title="Τιμή / Ποσοστό Επιτυχίας",
+        title=f"Tο Πρόβλημα του Γραμματέα (n={n}, τ={tau}) (Success Rates). Διάφορες τιμές του θ.",
+        xaxis_title="Error Rate",
+        yaxis_title="Success Rate",
         template="plotly_white",
         hovermode="closest" 
     )
